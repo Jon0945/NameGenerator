bd953181ce5142231130f8741885c56bb2089ed0
jon0945 <john.gullne@gmail.com>
2019-10-15 10:04:04 +0200

    using scanner to read a file

--- src/main/java/se/lexicon/files/App.java
+++ src/main/java/se/lexicon/files/App.java
@@ -1,9 +1,40 @@
 package se.lexicon.files;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {}
 
+    private static void scannerReadLastNameFile() {
+        Scanner scanfile = null;
+        Path filePath = Paths.get("lastnames.txt");
+        List<String> lastnameslist = new ArrayList<>();
+
+        try {
+            scanfile = new Scanner(filePath); //open file
+
+            while (scanfile.hasNext()) //end of file
+            {
+                lastnameslist.add(scanfile.nextLine()); //read line in file
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+        finally {
+            try {
+                scanfile.close(); //never forget to close file
+            }
+            catch (NullPointerException ee) {
+                System.out.println(ee);
+            }
+        }
+        lastnameslist.forEach(System.out::println);
     }
 }
